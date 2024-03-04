c98a6bd126dcb7bc5bc2596eb6ba7219f480e7b1
malavikaac <acmalavika@gmail.com>
2024-03-04 15:14:34 +0530

    feat - navigationrail

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,74 +8,55 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  // initialize a index
+  int _selectedIndex = 0;
 
-   // initialize a index 
-  int _selectedIndex = 0; 
-  
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:  Row( 
-        children: <Widget>[ 
-          // create a navigation rail 
-          NavigationRail( 
-            
-            selectedIndex: _selectedIndex, 
-            onDestinationSelected: (int index) { 
-              setState(() { 
-                _selectedIndex = index; 
-              }); 
-            }, 
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          // create a navigation rail
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
             extended: true,
             indicatorColor: Colors.blueGrey,
-          // labelType: NavigationRailLabelType.all, 
-            backgroundColor: Colors.blueGrey, 
-            destinations: const <NavigationRailDestination> 
-
-            [ 
-              // navigation destinations 
-              NavigationRailDestination( 
-                icon: Icon(Icons.favorite_border), 
-                selectedIcon: Icon(Icons.favorite), 
-                label: Text('Wishlist' , 
+            backgroundColor: Colors.blueGrey,
+            destinations: const <NavigationRailDestination>[
+              // navigation destinations
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite_border),
+                selectedIcon: Icon(Icons.favorite),
+                label: Text(
+                  'Wishlist',
                 ),
-                
-                
-              ), 
-              NavigationRailDestination( 
-                icon: Icon(Icons.person_outline_rounded), 
-                selectedIcon: Icon(Icons.person), 
-                label: Text('Account'), 
-              ), 
-            //    NavigationRailDestination(
-            // icon: Icon(Icons.mail),
-            // selectedIcon: Icon(Icons.mail),
-            // label: Row(
-            //   children: [
-            //      icon: Icon(Icons.mail),
-            //     Padding(
-            //       padding: EdgeInsets.only(left: 8.0),
-            //       child: Text('Messages'),
-            //     ),
-            //   ],
-            // ), )
-               
-            ], 
-            selectedIconTheme: const IconThemeData(color: Colors.white), 
-            unselectedIconTheme: const IconThemeData(color: Colors.black), 
-            selectedLabelTextStyle: const TextStyle(color: Colors.white), 
-          ), 
-           
-          Expanded( 
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.person_outline_rounded),
+                selectedIcon: Icon(Icons.person),
+                label: Text('Account'),
+              ),
+            ],
+            selectedIconTheme: const IconThemeData(color: Colors.white),
+            unselectedIconTheme: const IconThemeData(color: Colors.black),
+            selectedLabelTextStyle: const TextStyle(color: Colors.white),
+          ),
+
+          Expanded(
             child: Container(
               color: const Color.fromARGB(255, 218, 218, 218),
-              child: Center( 
-                child: Text('Page Number: $_selectedIndex'), 
+              child: Center(
+                child: Text('Page Number: $_selectedIndex'),
               ),
-            ), 
-          ) 
-        ], 
-      ), 
+            ),
+          )
+        ],
+      ),
     );
   }
 }
